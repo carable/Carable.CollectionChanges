7efe88c69fa2a505f09eb0acab0e9a441ff08554
Oskar Gewalli <oskar.gewalli@garantibil.se>
2017-03-31 14:37:11 +0200

    No need for specific type for collection changes from same type

--- Tests/Given_an_existing_list_with_one_element.cs
+++ Tests/Given_an_existing_list_with_one_element.cs
@@ -13,21 +13,21 @@ namespace Tests
         public void When_there_is_an_incoming_element()
         {
             Assert.Equal(existing.CollectionChanges(Cons(2, existing), t => t),
-                new CollectionChanges<int>(toBeAdded: new int[] { 2 }, toBeRemoved: new int[0], toChange: new[] { new CollectionItemsThatMayHaveChanged<int>(1, 1) }));
+                new CollectionChanges<int,int>(toBeAdded: new int[] { 2 }, toBeRemoved: new int[0], toChange: new[] { new CollectionItemsThatMayHaveChanged<int, int>(1, 1) }));
         }
 
         [Fact]
         public void When_there_an_empty_incoming_list()
         {
             Assert.Equal(existing.CollectionChanges(new int[0], t => t),
-                new CollectionChanges<int>(toBeAdded: new int[0], toBeRemoved: new int[] { 1 }, toChange: new CollectionItemsThatMayHaveChanged<int>[0]));
+                new CollectionChanges<int, int>(toBeAdded: new int[0], toBeRemoved: new int[] { 1 }, toChange: new CollectionItemsThatMayHaveChanged<int, int>[0]));
         }
 
         [Fact]
         public void When_incoming_is_the_same_as_existing()
         {
             Assert.Equal(existing.CollectionChanges(existing, t => t),
-                new CollectionChanges<int>(toBeAdded: new int[0], toBeRemoved: new int[0], toChange: new[] { new CollectionItemsThatMayHaveChanged<int>(1, 1) }));
+                new CollectionChanges<int, int>(toBeAdded: new int[0], toBeRemoved: new int[0], toChange: new[] { new CollectionItemsThatMayHaveChanged<int, int>(1, 1) }));
         }
 
         private IEnumerable<T> Cons<T>(T v, T[] existing)
